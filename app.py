2c1636004cdfbce70b0e6b4baac9b6c2c9d3f5fd
tranphuc2026abc-prog <tranphuc2026abc@gmail.com>
2026-04-18 16:16:55 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_title="🌟 Siêu Sao Lớp 2",
     page_icon="🌟",
     layout="centered",
-    initial_sidebar_bar="collapsed",
+    initial_sidebar_state="collapsed",
 )
 
 # ══════════════════════════════════════════════════════════════════════════════
